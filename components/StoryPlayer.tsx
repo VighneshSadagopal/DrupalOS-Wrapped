f2407985762a711ec0b905a6dd991ec0a63924a6
RChaubey16 <ruturajchaubey16@gmail.com>
2025-12-19 21:09:19 +0530

    final new change

--- components/StoryPlayer.tsx
+++ components/StoryPlayer.tsx
@@ -1252,16 +1252,12 @@ const StoryPlayer: React.FC<StoryPlayerProps> = ({ data, onExit }) => {
   const handleCopyLink = (e: React.MouseEvent) => {
     e.stopPropagation();
 
-    console.log(`HEYEYEYE`)
-
     const origin = window.location.origin;
     const userPath = `/user/${data.username}`;
 
-    const url = window.location.pathname.startsWith("/user/")
+    const url = window.location.pathname.startsWith('/user/')
       ? window.location.href
       : `${origin}${userPath}`;
-
-    console.log(`URL`, url)
 
     navigator.clipboard.writeText(url);
 
